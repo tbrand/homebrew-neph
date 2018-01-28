49b7fc2c159217073cef8fbb2ff9dd22e23266dc
Márton Szabó <notramo@vipmail.hu>
2018-01-28 15:33:11 +0100

    v0.1.21

--- neph.rb
+++ neph.rb
@@ -1,8 +1,8 @@
 class Neph < Formula
   desc "A modern command line job processor, similar with make command"
   homepage "https://tbrand.github.io/neph/"
-  url "https://github.com/tbrand/neph/releases/download/v0.1.19/neph"
-  sha256 "9d0658bacba4a36c1cdc27b4f5180c506da407bb96d9a06b1e135452ae617b09"
+  url "https://github.com/tbrand/neph/releases/download/v0.1.21/neph"
+  sha256 "bd41ba5272683dff11966905b0e25c2b1dc5eb5c449ade2e4472d6aa041aaeb8"
   # openssl dgst -sha256 path_to/neph
 
   depends_on "libyaml"
